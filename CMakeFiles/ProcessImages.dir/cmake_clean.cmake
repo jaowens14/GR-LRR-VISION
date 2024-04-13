f2b90df968f5238785101dd9ead1097516126c3c
jowens4 <jowens@avidpd.com>
2024-04-12 23:02:19 -0600

    upload cpp

---
+++ CMakeFiles/ProcessImages.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProcessImages.dir/ProcessImages.cpp.o"
+  "CMakeFiles/ProcessImages.dir/ProcessImages.cpp.o.d"
+  "ProcessImages"
+  "ProcessImages.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProcessImages.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/ProcessImages.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ProcessImages.
+# This may be replaced when dependencies are built.
